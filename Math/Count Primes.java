bf9b53dda8e71beeebffe45fc2d27c383d3e6762
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-04-29 20:12:24 +0530

    Create Count Primes.java
    Problem: 204. Count Primes
    Platform: LeetCode
    Approach: Sieve of Eratosthenes
    Time Complexity: O(n log log n)
    Space Complexity: O(n)

---
+++ Math/Count Primes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count=0;
+        boolean[] prime=new boolean[n+1];
+        for(int i=2;i<n;i++){
+            if(!prime[i]){
+                count++;
+                for(int j=i*i;j<n;j=j+i){
+                    prime[j]=true;
+                }
+            }
+        }
+        return count;
+    }
+}
