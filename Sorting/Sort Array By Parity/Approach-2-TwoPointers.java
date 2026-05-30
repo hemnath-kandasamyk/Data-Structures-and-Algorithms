e4403d2b92c50db4763db049e1a6888b70fe5355
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-05-30 16:29:41 +0530

    Create Approach-2-TwoPointers.java

---
+++ Sorting/Sort Array By Parity/Approach-2-TwoPointers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int start = 0, end = nums.length-1;
+        while (start < end){
+             if(nums[start]%2==0){
+                start++;
+             }
+             else if(nums[end]%2!=0){
+                end--;
+             }
+             else{
+                int temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                start++;
+                end--;
+             }
+        }
+        return nums;
+    }
+}
