edf8c7a22678ffaaefdf276a93072ce00064d48a
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-07-13 19:52:15 +0530

    Implement twoSum method to find pair with target sum

---
+++ Arrays/Two Sum - Pair with Given Sum/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(target-arr[i])){
+                return true;
+            }
+            map.put(arr[i],i);
+        }
+        return false;
+    }
+}
